daab15768a3d8316d1ffa6bc3e8e74fb44978522
xieshufan <xieshufan@idwzx.com>
2019-04-24 17:35:10 +0800

    添加检查工具

--- SettingTool/SettingUI/Setting.py
+++ SettingTool/SettingUI/Setting.py
@@ -1,5 +1,7 @@
 # coding=utf8
 
+import sys
+from PyQt5.QtWidgets import QApplication
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import pyqtSlot
 from SettingTool.SettingUI.SettingUI import Ui_Setting
@@ -14,3 +16,18 @@ class Setting(QtWidgets.QWidget, Ui_Setting):
 
         # 创建界面
         self.setupUi(self)  # 创建界面
+
+
+if __name__ == '__main__':
+    # 创建事件
+    app = QApplication(sys.argv)
+
+    # 创建ui
+    ui = Setting()
+
+    # 显示ui界面
+    # ui.showMaximized()
+    ui.show()
+
+    # 进入消息循环
+    sys.exit(app.exec_())
